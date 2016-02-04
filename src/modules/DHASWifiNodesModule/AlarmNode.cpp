819034d8cc93f361fd59ca8c9dc435020ebcb140
pat <git@dumaisnet.ca>
2016-02-04 16:13:08 -0500

    Final save before removing AlarmNode

--- src/modules/DHASWifiNodesModule/AlarmNode.cpp
+++ src/modules/DHASWifiNodesModule/AlarmNode.cpp
@@ -4,6 +4,7 @@ REGISTER_NODE_NAME(AlarmNode,"ESP8266 ALARM PGM");
 
 AlarmNode::AlarmNode()
 {
+    mPgms = 0;
 }
 
 AlarmNode::~AlarmNode()
@@ -16,10 +17,11 @@ bool AlarmNode::processData(char* buf, size_t size, Dumais::JSON::JSON& json)
 
     if (buf[0] == 'R' || buf[0]=='S')
     {
+        mPgms = buf[1];
         json.addValue("alarmnode","node");
+        if (buf[0] == 'S') json.addValue(1,"explicit");
         json.addList("pgms");
         for (int i = 0; i < 8; i++) json["pgms"].addValue(((buf[1]>>i)&1));
-
         return true;
     }
 
@@ -28,6 +30,20 @@ bool AlarmNode::processData(char* buf, size_t size, Dumais::JSON::JSON& json)
 
 void AlarmNode::registerCallBacks(ThreadSafeRestEngine* pEngine)
 {
+    RESTCallBack *p;
+
+    p = new RESTCallBack(this,&AlarmNode::status_callback,"Get PGM status");
+    pEngine->addCallBack("/dwn/"+mInfo.id+"/status","GET",p);
+    mRestCallbacks.push_back(p);
 }
 
+void AlarmNode::status_callback(RESTContext* context)
+{
+    RESTParameters* params = context->params;
+    Dumais::JSON::JSON& json = context->returnData;
+
+    json.addValue("ok","status");
+    json.addList("pgms");
+    for (int i = 0; i < 8; i++) json["pgms"].addValue(((mPgms>>i)&1));
+}
 
--- src/modules/DHASWifiNodesModule/AlarmNode.h
+++ src/modules/DHASWifiNodesModule/AlarmNode.h
@@ -4,10 +4,15 @@
 
 class AlarmNode: public IDWN
 {
+private:
+    uint8_t mPgms;
+
 public:
     AlarmNode();
     ~AlarmNode();
 
     bool processData(char* buf, size_t size, Dumais::JSON::JSON& reply);
     void registerCallBacks(ThreadSafeRestEngine* pEngine);
+
+    void status_callback(RESTContext* context);
 };
